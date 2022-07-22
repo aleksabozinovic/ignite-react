80c9ab419411044a1a67613ef89ff63e559488c4
aleksabozinovic <aleksabozinovic722@gmail.com>
2022-07-22 13:45:22 +0200

    Fetching data

--- src/api.js
+++ src/api.js
@@ -1,5 +1,3 @@
-require("dotenv").config();
-
 // Base URL
 const base_url = `https://api.rawg.io/api/`;
 
@@ -27,5 +25,11 @@ const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 // Popular Games
 const popular_games = `games?key=${process.env.REACT_APP_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+// Upcoming games
+const upcoming_games = `games?key=${process.env.REACT_APP_KEY}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+// New Games
+const new_games = `games?key=${process.env.REACT_APP_KEY}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
 export const popularGamesURL = () => `${base_url}${popular_games}`;
+export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
+export const newGamesURL = () => `${base_url}${new_games}`;
