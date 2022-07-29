36bad8549fb6306d739851463c58471dad4ae6b2
aleksabozinovic <aleksabozinovic722@gmail.com>
2022-07-29 11:07:14 +0200

    adding isLoading

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -25,37 +25,41 @@ const GameDetail = () => {
   const { screen, game, isLoading } = useSelector((state) => state.detail);
 
   return (
-    <CardShadow onClick={exitDetailHandler} className="card-shadow">
-      <Detail className="detail">
-        <Stats className="stats">
-          <div className="rating">
-            <h3>{game.name}</h3>
-            <p>Rating {game.rating}</p>
-          </div>
-          <Info className="info">
-            <h3>Platform</h3>
-            <Platforms className="platforms">
-              {game.platforms &&
-                game.platforms.map((data) => (
-                  <h3 key={data.platform.id}>{data.platform.name}</h3>
+    <>
+      {!isLoading && (
+        <CardShadow onClick={exitDetailHandler} className="card-shadow">
+          <Detail className="detail">
+            <Stats className="stats">
+              <div className="rating">
+                <h3>{game.name}</h3>
+                <p>Rating {game.rating}</p>
+              </div>
+              <Info className="info">
+                <h3>Platform</h3>
+                <Platforms className="platforms">
+                  {game.platforms &&
+                    game.platforms.map((data) => (
+                      <h3 key={data.platform.id}>{data.platform.name}</h3>
+                    ))}
+                </Platforms>
+              </Info>
+            </Stats>
+            <Media className="media">
+              <img src={game.background_image} alt="image" />
+            </Media>
+            <Description className="description">
+              <p>{game.description_raw}</p>
+            </Description>
+            <div className="gallery">
+              {screen.results &&
+                screen.results.map((screen) => (
+                  <img src={screen.image} alt="" key={screen.id} />
                 ))}
-            </Platforms>
-          </Info>
-        </Stats>
-        <Media className="media">
-          <img src={game.background_image} alt="image" />
-        </Media>
-        <Description className="description">
-          <p>{game.description_raw}</p>
-        </Description>
-        <div className="gallery">
-          {screen.results &&
-            screen.results.map((screen) => (
-              <img src={screen.image} alt="" key={screen.id} />
-            ))}
-        </div>
-      </Detail>
-    </CardShadow>
+            </div>
+          </Detail>
+        </CardShadow>
+      )}
+    </>
   );
 };
 
